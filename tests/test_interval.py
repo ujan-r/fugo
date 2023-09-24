1e0018384801b1c54a90941edb153d4d6dade92d
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2023-09-24 08:52:35 -0500

    Fix typo (`DIMINSHED` -> `DIMINISHED`)

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -5,7 +5,7 @@ from fugo.interval import Quality, Size
 def test_parsing():
     expected = {
         'P5': Interval.from_attrs(Quality.PERFECT, Size.FIFTH),
-        ' d10': Interval.from_attrs(Quality.DIMINSHED, Size.THIRD),
+        ' d10': Interval.from_attrs(Quality.DIMINISHED, Size.THIRD),
         'm3  ': Interval.from_attrs(Quality.MINOR, Size.THIRD),
         'M3': Interval.from_attrs(Quality.MAJOR, Size.THIRD),
         'a8': Interval.from_attrs(Quality.AUGMENTED, Size.OCTAVE),
