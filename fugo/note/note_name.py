f227779532bbd5e2b835ffa387b334201743509d
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2023-11-25 11:09:11 -0600

    Replace default constructor for `NoteName`

--- fugo/note/note_name.py
+++ fugo/note/note_name.py
@@ -9,3 +9,27 @@ from .internals import Accidental, LetterName
 class NoteName:
     letter: LetterName
     accidental: Accidental
+
+    def __init__(self, name: str):
+        copy = self.from_string(name)
+
+        self.letter = copy.letter
+        self.accidental = copy.accidental
+
+    @classmethod
+    def from_string(cls, name: str):
+        name = name.strip()
+
+        letter = LetterName.from_string(name[0])
+        accidental = Accidental.from_string(name[1:])
+
+        return cls.from_attrs(letter, accidental)
+
+    @classmethod
+    def from_attrs(cls, letter: LetterName, accidental: Accidental):
+        obj = super().__new__(cls)
+
+        obj.letter = letter
+        obj.accidental = accidental
+
+        return obj
