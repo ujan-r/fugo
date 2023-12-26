20df04507ddba1aeb1436108b1024dc9fe486d78
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2023-12-26 01:10:50 -0600

    Add `Chord` parsing tests

--- tests/test_chord.py
+++ tests/test_chord.py
@@ -1,21 +1,62 @@
 from fugo import Chord, Quality, NoteName
 
 
+def test_parsing():
+    expected = {
+        Chord('BM'): Chord.from_attrs(NoteName('B'), Quality.MAJOR),
+        Chord('Cmaj/E'): Chord.from_attrs(NoteName('C'), Quality.MAJOR, 1),
+        Chord('D'): Chord.from_attrs(NoteName('D'), Quality.MAJOR),
+        Chord('C#m'): Chord.from_attrs(NoteName('C#'), Quality.MINOR),
+        Chord('Emin'): Chord.from_attrs(NoteName('E'), Quality.MINOR),
+        Chord('F-'): Chord.from_attrs(NoteName('F'), Quality.MINOR),
+        Chord('F♯dim'): Chord.from_attrs(NoteName('F#'), Quality.DIMINISHED),
+        Chord('G♭º'): Chord.from_attrs(NoteName('Gb'), Quality.DIMINISHED),
+        Chord('Go/Db'): Chord.from_attrs(NoteName('G'), Quality.DIMINISHED, 2),
+        Chord('G#aug'): Chord.from_attrs(NoteName('G#'), Quality.AUGMENTED),
+        Chord('Abb+'): Chord.from_attrs(NoteName('Abb'), Quality.AUGMENTED),
+        Chord('A7'): Chord.from_attrs(NoteName('A'), Quality.MAJ_MIN_7),
+        Chord('A#Mm7/G#'): Chord.from_attrs(NoteName('A#'), Quality.MAJ_MIN_7, 3),
+        Chord('Dmaj7'): Chord.from_attrs(NoteName('D'), Quality.MAJ_7),
+        Chord('FbM7'): Chord.from_attrs(NoteName('Fb'), Quality.MAJ_7),
+        Chord('FΔ'): Chord.from_attrs(NoteName('F'), Quality.MAJ_7),
+        Chord('GΔ7/D'): Chord.from_attrs(NoteName('G'), Quality.MAJ_7, 2),
+        Chord('Gmin7'): Chord.from_attrs(NoteName('G'), Quality.MIN_7),
+        Chord('Em7'): Chord.from_attrs(NoteName('E'), Quality.MIN_7),
+        Chord('Bb-7'): Chord.from_attrs(NoteName('Bb'), Quality.MIN_7),
+        Chord('CmM7/Eb'): Chord.from_attrs(NoteName('C'), Quality.MIN_MAJ_7, 1),
+        Chord('C#-M7'): Chord.from_attrs(NoteName('C#'), Quality.MIN_MAJ_7),
+        Chord('F-Δ7/E♮'): Chord.from_attrs(NoteName('F'), Quality.MIN_MAJ_7, 3),
+        Chord('Bbminmaj7'): Chord.from_attrs(NoteName('Bb'), Quality.MIN_MAJ_7),
+        Chord('Ebo7'): Chord.from_attrs(NoteName('Eb'), Quality.DIM_7),
+        Chord('Aº7'): Chord.from_attrs(NoteName('A'), Quality.DIM_7),
+        Chord('Ddim7/Ab'): Chord.from_attrs(NoteName('D'), Quality.DIM_7, 2),
+        Chord('Gø'): Chord.from_attrs(NoteName('G'), Quality.HALF_DIM_7),
+        Chord('Eø7'): Chord.from_attrs(NoteName('E'), Quality.HALF_DIM_7),
+        Chord('Cm7b5'): Chord.from_attrs(NoteName('C'), Quality.HALF_DIM_7),
+        Chord('Am7♭5'): Chord.from_attrs(NoteName('A'), Quality.HALF_DIM_7),
+        Chord('Fmin7b5/Cb'): Chord.from_attrs(NoteName('F'), Quality.HALF_DIM_7, 2),
+        Chord('Dbmin7♭5'): Chord.from_attrs(NoteName('Db'), Quality.HALF_DIM_7),
+    }
+
+    for parsed, correct in expected.items():
+        assert parsed == correct
+
+
 def test_members():
     def note_names(s: str):
         return [NoteName(n) for n in s.split()]
 
     expected = {
-        Chord(NoteName('A'), Quality.MAJOR): note_names('A C# E'),
-        Chord(NoteName('C#'), Quality.MINOR, 1): note_names('E G# C#'),
-        Chord(NoteName('G'), Quality.DIMINISHED, 2): note_names('Db G Bb'),
-        Chord(NoteName('Cb'), Quality.AUGMENTED): note_names('Cb Eb G'),
-        Chord(NoteName('C'), Quality.MAJ_MIN_7, 3): note_names('Bb C E G'),
-        Chord(NoteName('E'), Quality.MAJ_7): note_names('E G# B D#'),
-        Chord(NoteName('Eb'), Quality.MIN_7): note_names('Eb Gb Bb Db'),
-        Chord(NoteName('D'), Quality.MIN_MAJ_7): note_names('D F A C#'),
-        Chord(NoteName('F'), Quality.DIM_7): note_names('F Ab Cb Ebb'),
-        Chord(NoteName('B'), Quality.HALF_DIM_7): note_names('B D F A'),
+        Chord('A'): note_names('A C# E'),
+        Chord('C#m/E'): note_names('E G# C#'),
+        Chord('Gdim/Db'): note_names('Db G Bb'),
+        Chord('Cb+'): note_names('Cb Eb G'),
+        Chord('C7/Bb'): note_names('Bb C E G'),
+        Chord('EM7'): note_names('E G# B D#'),
+        Chord('Ebm7'): note_names('Eb Gb Bb Db'),
+        Chord('DmM7'): note_names('D F A C#'),
+        Chord('Fo7'): note_names('F Ab Cb Ebb'),
+        Chord('Bø'): note_names('B D F A'),
     }
 
     for chord, names in expected.items():
